6d85731e6295e429eb19f22dbead35a38a29fe35
10908 <hongyan.li@hand-china.com>
2020-05-01 17:44:09 +0800

    [update] eureka config

--- service/src/main/java/service/ServiceApplication.java
+++ service/src/main/java/service/ServiceApplication.java
@@ -2,11 +2,13 @@ package service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author lhongyan
  * @since 0.0.1
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceApplication {
 
